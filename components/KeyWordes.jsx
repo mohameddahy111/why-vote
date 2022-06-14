f9def5d261020683039503f8dcc29b3531720140
mohameddahy111 <mohameddahy111@gmail.com>
2022-06-14 04:08:11 +0200

    fixed keyWordes

--- components/KeyWordes.jsx
+++ components/KeyWordes.jsx
@@ -1,53 +1,74 @@
-import { List, ListItem } from '@mui/material';
-import React, { useState } from 'react';
+import { Remove } from '@mui/icons-material';
+import { IconButton, List, ListItem } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import styles from '../styles/keyWordes.module.css';
 
 const KeyWordes = () => {
-  const keyWordes =[
-    'key',
-    'key1',
-    'computer',
-    'laptop',
-  ];
+  const [inputList, setInputList] = useState([]);
+  const [keyWordes, setKeyWordes] = useState('');
 
-  const [query, setQuery] = useState('');
-  const updateQuery = query => {
-    setQuery(query.trim());
+  const handelChenge = e => {
+    const { value } = e.target;
+    setKeyWordes(value);
   };
-  const showingKeyWords =
-    query === ''
-      ? keyWordes
-      : keyWordes.filter(c =>
-          c.toLowerCase().includes(query.toLowerCase())
-        );
+  const handelSubmit = e => {
+    e.preventDefault();
+    if (keyWordes === '') {
+      alert('Please enter a key word');
+    } else {
+      setInputList([...inputList, keyWordes]);
+      setKeyWordes('');
+    }
+  };
+  const handelRemove = index => {
+    const newList = [...inputList];
+    newList.splice(index, 1);
+    setInputList(newList);
+  };
+  useEffect(() => {
+    localStorage.setItem('keyWordes', JSON.stringify(inputList));
+  }, [inputList]);
 
   return (
-    <div>
-      <div className={styles.contanier}>
-      <p>Add new keyWordes</p>
-      <div className={styles.box}>
-
-        <input
-        className={styles.input}
-        placeholder="keyWordes"
-          type='text'
-          name=''
-          id=''
-          value={query}
-          onChange={e => updateQuery(e.target.value)}
-        />
-        <button className={styles.addButton}>Add</button>
+    (
+      <div>
+        <div className={styles.contanier}>
+          <p>Add new keyWordes</p>
+          <form onSubmit={handelSubmit} method='post'>
+            <div className={styles.box}>
+              <input
+                className={styles.input}
+                placeholder='keyWordes'
+                type='text'
+                value={keyWordes}
+                name='keyWordes'
+                id=''
+                onChange={handelChenge}
+              />
+              <button type='submit' className={styles.addButton}>
+                Add
+              </button>
+            </div>
+          </form>
+          <p>suggested keywords</p>
+        </div>
+        <div className={styles.keyWordes}>
+          <List className={styles.list}>
+            {inputList.map((keyWord, index) => (
+              <div key={index} style={{ display: 'flex', gap: '5px' }}>
+                <ListItem key={index}>
+                  {keyWord}
+                  <IconButton onClick={() => handelRemove(index)}>
+                    <Remove />
+                  </IconButton>
+                </ListItem>
+                <p>,</p>
+              </div>
+            ))}
+          </List>
+        </div>
       </div>
-        <p>suggested keywords</p>
-      </div>
-      <div className={styles.keyWordes}>
-        {showingKeyWords.map((keyWord, index) => {
-          return <div key={index} > <List  className={styles.list} >
-            <ListItem  onClick={()=>{setQuery(keyWord)}} >{keyWord}</ListItem>
-            </List></div>
-        })}
-      </div>
-    </div>
+    )
   );
 };
 
